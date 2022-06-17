39d524b437086a2a88e6137405c16b21cf79479b
Ezequiel Lo Iacono <ezequiel_lo_iacono@pm.me>
2022-06-17 10:29:52 +0200

    feat(style)added spaces

--- src/app/pokemons.service.spec.ts
+++ src/app/pokemons.service.spec.ts
@@ -5,15 +5,18 @@ import { PokemonsService } from './pokemons.service';
 
 describe('PokemonsService', () => {
   let service: PokemonsService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule]
     });
     service = TestBed.inject(PokemonsService);
   });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
   it('should be bulbasaur', (done) => {
     service.getPokemon(1).subscribe(data => {
       expect(data.name).toBe('bulbasaur');
